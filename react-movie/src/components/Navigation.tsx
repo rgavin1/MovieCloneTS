5f1c58fdaa6a3f3eee1494d20dded9858fcd3d6d
Ramsey Gavin <ramseygavin@Ramsey-Gavin.local>
2021-07-21 21:01:43 -0700

    pages are linked

--- react-movie/src/components/Navigation.tsx
+++ react-movie/src/components/Navigation.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation: React.FC = () => {
-  return <div>Navigation</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+        <li>
+          <Link to="/tv">Tv</Link>
+        </li>
+        <li>
+          <Link to="/movie">Movie</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
