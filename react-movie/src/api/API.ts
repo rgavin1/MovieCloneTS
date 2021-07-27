452ac7645aaac5b78713f4de93b805297c101632
Ramsey Gavin <ramseygavin@Ramsey-Gavin.local>
2021-07-26 22:08:38 -0700

    god damn API call

---
+++ react-movie/src/api/API.ts
@@ -0,0 +1,52 @@
+const APIKEY = "16a2d758fecccf61be2a8107d67a5aba";
+
+export type RawResults = {
+  page: number;
+  results: string[];
+  total_pages: number;
+  total_results: number;
+};
+
+type Movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[] | number | null;
+  id: string;
+  media_type: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: string;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+type Tv = Movie & {
+  first_air_date: string;
+  name: string;
+  origin_country: string[];
+  original_name: string;
+};
+
+export enum MediaTypes {
+  MOVIE = "movie",
+  TV = "tv",
+  ALL = "all",
+  PERSON = "person",
+}
+
+/**
+ * I dont this function returns the same fu*kin'
+ * function twice 🤯
+ */
+export const trendingMedia = async (mediaType: MediaTypes) => {
+  const response: any = await fetch(
+    `https://api.themoviedb.org/3/trending/${mediaType}/week?api_key=${APIKEY}`
+  );
+
+  return await response;
+};
--- react-movie/src/pages/Home.tsx
+++ react-movie/src/pages/Home.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 // API
 import { trendingMedia, MediaTypes } from "../api/API";
 
 const Home: React.FC = () => {
-  trendingMedia(MediaTypes.ALL);
+  // const [loading, setLoading] = useState<boolean>(false);
+  const [list, setList] = useState<any>([]);
 
-  return <div>Home</div>;
+  useEffect(() => {
+    // setLoading(true);
+    trendingMedia(MediaTypes.ALL)
+      .then((res) => res.json())
+      .then((data) => setList(data.results));
+    // setLoading(false);
+  }, []);
+
+  console.log("list ", list);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      {/* {loading && "List"} */}
+    </div>
+  );
 };
 
 export default Home;
