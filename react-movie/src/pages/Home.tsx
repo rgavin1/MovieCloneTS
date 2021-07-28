c8e8ba0b5ef46a8bdfac96b245bd4dded6d5d9ff
Ramsey Gavin <ramseygavin@Ramsey-Gavin.local>
2021-07-27 19:26:53 -0700

    homepage

--- react-movie/src/pages/Home.tsx
+++ react-movie/src/pages/Home.tsx
@@ -2,28 +2,26 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 
 // API
-import { trendingMedia, MediaTypes } from "../api/API";
+import { resquestTrendingMedia, MediaTypes } from "../api/API";
 
 const Home: React.FC = () => {
-  // const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
   const [list, setList] = useState<any>([]);
 
   useEffect(() => {
-    // setLoading(true);
-    trendingMedia(MediaTypes.ALL)
-      .then((res) => res.json())
-      .then((data) => setList(data.results));
-    // setLoading(false);
+    setLoading(true);
+    setTimeout(() => {
+      resquestTrendingMedia(MediaTypes.ALL)
+        .then((res) => res.json())
+        .then((data) => setList(data.results));
+
+      setLoading(false);
+    }, 5000);
   }, []);
 
   console.log("list ", list);
 
-  return (
-    <div>
-      <h1>Home</h1>
-      {/* {loading && "List"} */}
-    </div>
-  );
+  return <>{loading ? <h1>Loading...</h1> : <>List</>}</>;
 };
 
 export default Home;
