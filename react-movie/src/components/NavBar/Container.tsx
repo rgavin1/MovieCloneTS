36e598d57a4256013a6403d03f58938de9b57332
Brooke Gavin <brooklynn2497@Brookes-MacBook-Air.local>
2021-12-12 20:33:58 -0700

    Major updates to the navbar

--- react-movie/src/components/NavBar/Container.tsx
+++ react-movie/src/components/NavBar/Container.tsx
@@ -1,23 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faSearch, faFilm, faTv, faTheaterMasks, faUser } from '@fortawesome/free-solid-svg-icons'
 
 /** TODO: Page this into types.ts */
-type Page = { name: string; pathname: string; }
+type Page = { name: string; pathname: string; icon?: any; }
 
 /** FIXME: Create a list to store all menu-items (links to page)*/
 export const pages: Page[] = [
-  { name: "Home", pathname: "" },
-  { name: "Search", pathname: "search" },
-  { name: "Tv", pathname: "tv" },
-  { name: "Movies", pathname: "movies" },
+  { name: "Home", pathname: "", icon: faHome },
+  { name: "Search", pathname: "search", icon: faSearch },
+  { name: "Tv", pathname: "tv", icon: faFilm },
+  { name: "Movies", pathname: "movies", icon: faTv },
 ]
 
 
-export const Logo: React.FC = () => <div>Logo</div>
-export const Profile: React.FC = () => <div id="profile">Profile</div>
+export const Logo: React.FC = () => <div id="logo">
+  <Link to="/">
+    <FontAwesomeIcon icon={faTheaterMasks} />
+  </Link>
+</div>
+
+export const Profile: React.FC = () => <div id="profile"><FontAwesomeIcon icon={faUser} /></div>
 
 export const MenuItem: React.FC<{ page: Page }> = ({ page }): JSX.Element =>
-  <Link to={`/${page.pathname}`}>{page.name}</Link>
+  <Link to={`/${page.pathname}`}>
+    <div className="menuItemIconContainer">
+      <FontAwesomeIcon icon={page.icon} /> <span className="showMenuItemName">{page.name}</span>
+    </div>
+  </Link>
 
 const Container: React.FC = () => {
   return (
--- react-movie/src/layouts/Content/Container.tsx
+++ react-movie/src/layouts/Content/Container.tsx
@@ -3,6 +3,12 @@ import React from 'react'
 const Container: React.FC = () => {
     return (
         <div className="contentContainer">
+            {/* Mock Hero */}
+            <div className="mockHero" style={{
+                width: "100%",
+                padding: "31%",
+                background: "pink"
+            }}></div>
             Content
         </div>
     )
