cad5aef115edd87dc140602e1303cfa8ec963b0b
Brooke Gavin <brooklynn2497@Brookes-MacBook-Air.local>
2021-12-18 14:58:26 -0700

    Pending - Working on hero + added poppins font

--- react-movie/src/components/Hero/Container.tsx
+++ react-movie/src/components/Hero/Container.tsx
@@ -4,7 +4,12 @@ import React from "react";
 const Container: React.FC = () => {
   return (
     <div id="hero" >
-
+      <div id="featureDescription">
+        <h1>Title</h1>
+        <p>feature Metadata</p>
+        <p>feature description</p>
+        <p>feature genre</p>
+      </div>
     </div>
   );
 };
